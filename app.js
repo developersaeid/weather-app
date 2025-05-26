94fa044916943c900042f3910e02bfb75707d0f5
Saeid Behjat <saeiddeveloper@yahoo.com>
2025-05-26 15:00:24 +0330

    location
    taking location information by user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const API_KEY = "c2a5578949b65ff259b14fd948053451";
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
+const locationIcon = document.getElementById("location");
 
 const getCurrentWeatherByName = async (city) => {
   const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -11,13 +12,23 @@ const getCurrentWeatherByName = async (city) => {
   const json = await response.json();
   return json;
 };
+// geographic coordinates location
+const getCurrentWeatherByCoordinates = async (lat, lon) => {
+  const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
+
 // showing the current weather of the city
 const renderCurrentWeather = (data) => {
   console.log(data);
-  const weatherJsx = `
+  const weather = `
     <h1>${data.name}, ${data.sys.country}</h1>
     <div id="main">
-        <img alt="weather icon" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" />
+        <img alt="weather icon" src="http://openweathermap.org/img/w/${
+          data.weather[0].icon
+        }.png" />
         <span>${data.weather[0].main}</span>
         <p>${Math.round(data.main.temp)} °C</p>
     </div>
@@ -27,10 +38,9 @@ const renderCurrentWeather = (data) => {
     </div>
 
     `;
-
-  weatherContainer.innerHTML = weatherJsx;
+  weatherContainer.innerHTML = weather;
 };
-
+// showing the current weather of the city
 const searchHandler = async () => {
   const cityName = searchInput.value;
   if (!cityName) {
@@ -39,4 +49,21 @@ const searchHandler = async () => {
   const currentData = await getCurrentWeatherByName(cityName);
   renderCurrentWeather(currentData);
 };
+// location
+const positionCallback = async (position) => {
+  const { latitude, longitude } = position.coords;
+  const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
+  renderCurrentWeather(currentData);
+};
+const errorCallback = (error) => {};
+const locationHandler = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(positionCallback, errorCallback);
+  } else {
+    alert("Your Browser does not support Geolocation!");
+  }
+};
+
+// event listeners
 searchButton.addEventListener("click", searchHandler);
+locationIcon.addEventListener("click", locationHandler);
